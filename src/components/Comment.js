d90066f5870b42d97bf9f239556e636eec1e0db9
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-10-26 15:32:57 -0700

    Class based component / state exercise done.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,11 +1,33 @@
-import React from 'react'; 
+import React, {Component} from 'react'; 
 
-const Comment = (props) => {
-    return (
-        <div>
-            <p>{props.message}</p>
-        </div>
-    )
-}
+class Comment extends Component {
+    state = {
+        karma: 'good'
+    }; 
+
+    changeKarma = () => {
+        if (this.state.karma === 'good') {
+            this.setState({
+                karma: 'bad'
+            })
+        } else {
+            this.setState({
+                karma: 'good'
+            })
+        }
+    }
+
+    render () {
+        let goodKarmaSrc = "https://media.giphy.com/media/1zJFtdNUhblPRT1yQy/giphy.gif";
+        let badKarmaSrc = "https://media.giphy.com/media/AxVvk4TmDfmoMtSmmQ/giphy.gif";
+        return (
+            <div className={this.state.karma}>
+                <img src={this.state.karma === 'bad' ? badKarmaSrc : goodKarmaSrc} />
+                <p>{this.props.message}</p>
+                <button className={'button'} onClick={this.changeKarma}>Change Karma</button>
+            </div>
+        )
+    }
+}; 
 
 export default Comment; 
